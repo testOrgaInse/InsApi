44e7a9b88176247597d337797da9b87e933b4e10
Nacim Goura <nacim.goura.dev@gmail.com>
2018-11-14 12:54:00 +0100

    add edit and create for structures page

--- bin/imports/community.js
+++ bin/imports/community.js
@@ -3,6 +3,6 @@ import pool from "./connexion_database";
 export const importCommunity = async () => {
   const result = await pool.query({
     sql: "INSERT INTO community (name, gate) VALUES ($name, $gate)",
-    parameters: { name: "INSERM", gate: "INSERM" }
+    parameters: { name: "INSERM", gate: "proxy" }
   });
 };
