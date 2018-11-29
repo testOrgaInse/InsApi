958db01502f69ae399983450c3c381ac00ccf1cf
Nacim Goura <nacim.goura.dev@gmail.com>
2018-11-29 14:01:48 +0100

    fix bug encoding file

--- bin/change_encoding.sh
+++ bin/change_encoding.sh
@@ -1,4 +1,4 @@
-for file in ../imports/*
+for file in imports/*
 do
     if file -i "$file" | grep -q "8859"; then
         iconv -f iso-8859-1 -t utf-8 "$file" > "$file.new" && mv -f "$file.new" "$file"
